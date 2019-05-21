01a8a58e5aa96f48c797f865d7d68c3b9f783e49
Miso <miso.stamenic@gmail.com>
2019-05-21 12:57:43 +0200

    added mapping Profile - dotnet

---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,18 @@
+using DockerDemo.Models;
+using AutoMapper;
+using employee.Controllers.Resources;
+
+namespace DockerDemo.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Mapping domain class to resource class.
+            CreateMap<Employee, EmployeeResource>();
+            //Mapping API resource class to domain class. And the id property is ignored to mapping.
+            CreateMap<EmployeeResource, Employee>()
+                .ForMember(v => v.Id, opt => opt.Ignore());
+        }
+    }
+}
