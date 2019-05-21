d37d80734edb822fa6c2014d65e7bc4f122a9e96
Miso <miso.stamenic@gmail.com>
2019-05-21 12:50:53 +0200

    added api resource

---
+++ Controllers/Resources/EmployeeResource.cs
@@ -0,0 +1,11 @@
+namespace employee.Controllers.Resources
+{
+    public class EmployeeResource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FatherName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
